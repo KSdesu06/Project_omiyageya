14048cd77edb9200d69fc8e70fc60d7c76e99083
KSdesu06 <sr.kunyanut_st@tni.ac.th>
2023-07-21 05:26:41 +0700

    Update authorize

--- routes/category.js
+++ routes/category.js
@@ -2,15 +2,17 @@ const express = require('express');
 const router = express.Router();
 const categoryController = require('../controllers/categoryController')
 const { body } = require('express-validator')
+const passportJWT = require('../middleware/passportJWT')
+const checkAdmin = require('../middleware/checkAdmin')
 
 router.get('/', categoryController.index)
 router.get('/:id', categoryController.show)
-router.post('/', [
+router.post('/', [passportJWT.isLogin,checkAdmin.isAdmin], [
     body('name').not().isEmpty().withMessage("Category name is required."),
     body('numOfProduct').isInt({min:0}).withMessage("Number of product must not be decimal number or negative number."),
 ], categoryController.insert) //validation
-router.delete('/:id', categoryController.destroy)
-router.put('/:id', [
+router.delete('/:id', [passportJWT.isLogin,checkAdmin.isAdmin], categoryController.destroy)
+router.put('/:id', [passportJWT.isLogin,checkAdmin.isAdmin], [
     body('name').not().isEmpty().withMessage("Category name is required."),
     body('numOfProduct').isInt({min:0}).withMessage("Number of product must not be decimal number or negative number."),
 ], categoryController.update)
--- routes/product.js
+++ routes/product.js
@@ -2,18 +2,20 @@ const express = require('express');
 const router = express.Router();
 const productController = require('../controllers/productController')
 const { body } = require('express-validator')
+const passportJWT = require('../middleware/passportJWT')
+const checkAdmin = require('../middleware/checkAdmin')
 
 router.get('/', productController.index)
 router.get('/:id', productController.show)
-router.post('/', [
+router.post('/', [passportJWT.isLogin,checkAdmin.isAdmin], [
     body('name').not().isEmpty().withMessage("Product name is required."),
     body('price').isFloat({min:0}).withMessage("Product price must not be negvative number."),
     body('quantity').isInt({min:0}).withMessage("Quantity of product must not be negative or decimal number."),
     body('statusOfProduct').not().isEmpty().withMessage("Product status is required.").isIn(['ready for shipment', 'pre-order', 'out of stock']).withMessage("Product status must be either 'ready for shipment', 'pre-order', 'out of stock'."),
     body('category').not().isEmpty().withMessage("Product category is required.")
 ], productController.insert) //validation
-router.delete('/:id', productController.destroy)
-router.put('/:id', [
+router.delete('/:id', [passportJWT.isLogin,checkAdmin.isAdmin], productController.destroy)
+router.put('/:id', [passportJWT.isLogin,checkAdmin.isAdmin], [
     body('name').not().isEmpty().withMessage("Product name is required."),
     body('price').isFloat({min:0}).withMessage("Product price must not be negvative number."),
     body('quantity').isInt({min:0}).withMessage("Quantity of product must not be negative or decimal number."),
